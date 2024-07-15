d455bceafb66c5ec7fabee7fd8966964624550c5
HydraponKing <momomolchenel@gmail.com>
2024-07-15 19:05:28 +0500

    0.6

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,22 +1,41 @@
-  import React from 'react';
+// src/App.jsx
+import React, { useState, useEffect } from 'react';
+import './App.css';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 import PopBrowse from './components/popups/PopBrowse/PopBrowse';
 import PopNewCard from './components/popups/PopNewCard/PopNewCard';
 import PopExit from './components/popups/PopExit/PopExit';
 import PopUser from './components/popups/PopUser/PopUser';
+import Loader from './components/Loader/Loader';
 
-import './App.css';
+function App() {
+  const [loading, setLoading] = useState(true);
 
-const App = () => (
-  <div className="App">
-    <Header />
-    <Main />
-    <PopBrowse />
-    <PopNewCard />
-    <PopExit />
-    {/*<PopUser /> */}
-  </div>
-);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000); // Имитация загрузки данных в 2 секунды
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <div className="wrapper">
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          <Header />
+          <Main />
+          <PopBrowse />
+          <PopNewCard />
+          <PopExit />
+          <PopUser />
+        </>
+      )}
+    </div>
+  );
+}
 
 export default App;
---
+++ vite-project/src/components/Loader/Loader.jsx
@@ -0,0 +1,13 @@
+// src/components/Loader/Loader.jsx
+import React from 'react';
+import './Loader.css';
+
+const Loader = () => {
+  return (
+    <div className="loader">
+      <p>Страница загружается...</p>
+    </div>
+  );
+};
+
+export default Loader;
