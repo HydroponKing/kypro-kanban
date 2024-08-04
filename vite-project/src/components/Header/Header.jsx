18e8e1420f7db1b3e19a6c6f92cd31de28eb8859
HydraponKing <momomolchenel@gmail.com>
2024-08-05 03:19:14 +0500

    header components-styled

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -1,7 +1,98 @@
-// src/components/Header/Header.jsx
 import React, { useState } from 'react';
-//import './Header.css';
+import styled from 'styled-components';
 import PopUser from '../popups/PopUser/PopUser';
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  margin: 0 auto;
+  background-color: #ffffff;
+`;
+
+const Container = styled.div`
+  max-width: 1260px;
+  width: 100%;
+  margin: 0 auto;
+  padding: 0 30px;
+`;
+
+const HeaderBlock = styled.div`
+  height: 70px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: space-between;
+  position: relative;
+  top: 0;
+  left: 0;
+  padding: 0 10px;
+`;
+
+const Logo = styled.div`
+  &._show._light img {
+    width: 85px;
+  }
+  &._dark img {
+    width: 85px;
+  }
+`;
+
+const HeaderNav = styled.nav`
+  max-width: 290px;
+  padding: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const HeaderButton = styled.button`
+  width: 178px;
+  height: 30px;
+  border-radius: 4px;
+  background-color: #565eef;
+  color: #ffffff;
+  border: none;
+  font-size: 14px;
+  line-height: 1;
+  font-weight: 500;
+  margin-right: 20px;
+  &:hover {
+    background-color: #33399b;
+  }
+  & a {
+    color: #ffffff;
+  }
+`;
+
+const HeaderUser = styled.div`
+  height: 20px;
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+  font-size: 14px;
+  line-height: 20px;
+  color: #565eef;
+  cursor: pointer;
+  &:hover {
+    color: #33399b;
+  }
+  &:hover::after {
+    border-left-color: #33399b;
+    border-bottom-color: #33399b;
+  }
+  &::after {
+    content: "";
+    display: block;
+    width: 6px;
+    height: 6px;
+    border-radius: 1px;
+    border-left: 1.9px solid #565eef;
+    border-bottom: 1.9px solid #565eef;
+    transform: rotate(-45deg);
+    margin: -6px 0 0 5px;
+    padding: 0;
+  }
+`;
 
 const Header = ({ onCardAdd }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -15,38 +106,38 @@ const Header = ({ onCardAdd }) => {
       id: Date.now(),
       topic: 'Web Design',
       title: 'Новая задача',
-      date: new Date().toISOString().split('T')[0], // текущая дата в формате YYYY-MM-DD
+      date: new Date().toISOString().split('T')[0],
       status: 'Без статуса'
     };
     onCardAdd(newCard);
   };
 
   return (
-    <header className="header">
-      <div className="container">
-        <div className="header__block">
-          <div className="header__logo _show _light">
+    <HeaderWrapper>
+      <Container>
+        <HeaderBlock>
+          <Logo className="header__logo _show _light">
             <a href="" target="_self">
               <img alt="logo" src="public/logo.png" />
             </a>
-          </div>  
-          <div className="header__logo _dark">
+          </Logo>
+          <Logo className="header__logo _dark">
             <a href="" target="_self">
               <img alt="logo" src="public/logo_dark.png" />
             </a>
-          </div>
-          <nav className="header__nav">
-            <button className="header__btn-main-new _hover01" id="btnMainNew" onClick={handleAddTaskClick}>
+          </Logo>
+          <HeaderNav>
+            <HeaderButton id="btnMainNew" onClick={handleAddTaskClick}>
               Создать новую задачу
-            </button>
-            <div className="header__user _hover02" onClick={handleUserClick}>
+            </HeaderButton>
+            <HeaderUser onClick={handleUserClick}>
               Ivan Ivanov
-            </div>
+            </HeaderUser>
             {isModalOpen && <PopUser onClose={handleUserClick} />}
-          </nav>
-        </div>
-      </div>
-    </header>
+          </HeaderNav>
+        </HeaderBlock>
+      </Container>
+    </HeaderWrapper>
   );
 };
 
