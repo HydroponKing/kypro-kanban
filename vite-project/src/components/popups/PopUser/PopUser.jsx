e9929e786fa6292bb0059d26c0561127b32c02c9
HydraponKing <momomolchenel@gmail.com>
2024-08-16 05:20:33 +0500

    Добавил в PopUser информацию о пользователя из api

--- vite-project/src/components/popups/PopUser/PopUser.jsx
+++ vite-project/src/components/popups/PopUser/PopUser.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './PopUser.css';
+import { UserContext } from '../../UserContext'; // Правильный путь к UserContext
 
 const PopUser = ({ onClose }) => {
   const navigate = useNavigate();
+  const { user } = useContext(UserContext); // Используем useContext для доступа к контексту пользователя
 
   const handleLogoutClick = () => {
     onClose(); // Закрыть модальное окно
@@ -13,12 +15,18 @@ const PopUser = ({ onClose }) => {
   return (
     <div className="user-modal">
       <div className="user-modal-content">
-        <p className="pop-user-set__name">Ivan Ivanov</p>
-        <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
-        <div className="pop-user-set__theme">
-          <p>Темная тема</p>
-          <input className="checkbox" name="checkbox" type="checkbox" />
-        </div>
+        {user ? (
+          <>
+            <p className="pop-user-set__name">{user.login}</p> {/* Отображаем логин пользователя */}
+            <p className="pop-user-set__mail">{`${user.login}@gmail.com`}</p> {/* Формируем email на основе логина */}
+            <div className="pop-user-set__theme">
+              <p>Темная тема</p>
+              <input className="checkbox" name="checkbox" type="checkbox" />
+            </div>
+          </>
+        ) : (
+          <p>Загрузка...</p>
+        )}
         <button type="button" onClick={handleLogoutClick}>Выйти</button>
       </div>
     </div>
